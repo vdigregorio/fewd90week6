24865b6a3b689979ff10a859ba4654794d6bb0cf
Hart Liddell <hart@leapingfish.io>
2016-08-04 21:15:26 -0400

    Harts changes

--- js/project.js
+++ js/project.js
@@ -1,60 +1,50 @@
 $(document).ready(function(){
 
-$("nav a").click(function(){
-$("nav a.current").removeClass("current");
-$(this).addClass("current");
+    $("nav a").click(function(){
+        $("nav a.current").removeClass("current");
+        $(this).addClass("current");
+
+        // var paragraphNum;
+        // if (document.getElementById("paragraphs").value != "") {
+        //     paragraphNum = document.getElementById("paragraphs").value = "";
+        // } else {
+        //     paragraphNum = 3;
+        // }
+        // What's below is shorthand for what is commented out above.
+        // It is called a ternary operator and is one of my favorite things!
+        var paragraphNum = document.getElementById("paragraphs").value != ""
+            ? document.getElementById("paragraphs").value != "" : 3;
+
+        generateIpsum(paragraphNum);
+    });
+
+    //generate ipsum, need # pgphs
+    function generateIpsum(num) {
+        var whichIpsum = $("nav a.current").attr("data-ipsum");//active ipsum
+        //dont forget to slice the children
+        $("#" + whichIpsum).children().slice(0, num).show(0);
+        //use slidedown for animation!
+        $("#" + whichIpsum).slideDown("slow");
+    }
+
+    //hide the ipsum when tabbing or clicking anywhere on screen!
+    function hideIpsum() {
+        var whichIpsum = $("nav a.current").attr("data-ipsum");
+        document.getElementById("paragraphs").value = ""; //empty textbox
+        //make text dissapear (better to slide up the class, rather than div with .current)
+        $(".ipsum").slideUp("slow", function() {
+            // Use the callback function to make this
+            // happen when animation is complete.
+            $(this).find('p').hide();
+        });
+    }
+
+    $("button").click(function(){
+        var numberP = document.getElementById("paragraphs").value;
+        generateIpsum(numberP);
+    });
+
+    $("#paragraphs").focus(function(){
+        hideIpsum();
+    });
 });
-//generate ipsum, need # pgphs
-function generateIpsum(num) {
-var whichIpsum = $("nav a.current").attr("data-ipsum");//active ipsum
-$("#" + whichIpsum).slideDown("slow"); //use slidedown for animation!
-$("#" + whichIpsum).children().slice(0, num).slideDown("slow");//dont forget to slice the children
-}
-//hide the ipsum when tabbing or clicking anywhere on screen!
-function hideIpsum() {
-var whichIpsum = $("nav a.current").attr("data-ipsum");
-document.getElementById("paragraphs").value = ""; //empty textbox
-$(".ipsum").slideUp("slow");//make text dissapear (better to slide up the class, rather than div with .current)
-}
-$("button").click(function(){
-var numberP = document.getElementById("paragraphs").value;
-generateIpsum(numberP);
-});
-$("#paragraphs").focus(function(){
-hideIpsum();
-});
-});
-
-
-			
-			
-				
-				
-			 
-			
-			
-
-
-		
-
-			
-			
-			
-
-			
-
-
-		
-		
-		
-
-
-
-
-
-
-
-
-
-
-
